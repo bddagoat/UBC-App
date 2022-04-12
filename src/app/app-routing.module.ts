17f884705ccea86c13be088d9003a2ec9a6151ac
bddagoat <davisb21@hsc.edu>
2022-04-11 22:00:04 -0400

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-
+  {path: '', component: HomeComponent},
+  {path: 'header', component: HeaderComponent},
 
 
   {path: '**', redirectTo: ''}
