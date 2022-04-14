6ed44fce4aa93f7e6dcc3d77f0d18c40c0e40911
bddagoat <davisb21@hsc.edu>
2022-04-13 20:33:25 -0400

    topNav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalendarComponent } from './calendar/calendar.component';
+import { ContactComponent } from './contact/contact.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'calendar', component: CalendarComponent},
+  {path: 'contact', component: ContactComponent},
   {path: 'header', component: HeaderComponent},
+  {path: 'services', component: ServicesComponent},
 
 
   {path: '**', redirectTo: ''}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    CalendarComponent,
+    ContactComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
